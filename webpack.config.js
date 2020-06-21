1085763434964b5324cfacd74e9e190fddb0ebf7
Marwen Trabelsi <marwen@pop-os.com>
2020-06-21 18:35:34 +0200

    [Project Setup] Installed basic TSX and Webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack'); 
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.tsx',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({template: './public/index.html'})
+  ]
+};
